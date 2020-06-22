857ac514689ded8403ad6784360b5c9ddb2261ae
blackjune67 <buea486@gmail.com>
2020-06-23 00:12:24 +0900

    게시글 수정 작업 중

--- src/main/java/com/june/springboot/service/posts/PostsService.java
+++ src/main/java/com/june/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.june.springboot.service.posts;
 
 import com.june.springboot.domain.posts.Posts;
 import com.june.springboot.domain.posts.PostsRepository;
+import com.june.springboot.web.dto.PostsListResponseDto;
 import com.june.springboot.web.dto.PostsResponeseDto;
 import com.june.springboot.web.dto.PostsSaveRequestDto;
 import com.june.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -25,19 +27,24 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
     public PostsResponeseDto findById(Long id) {
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
+
         return new PostsResponeseDto(entity);
     }
 
-    @Transactional(readOnly = true)
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc() {
-        return postsRepository.findAllDesc().stream().map(PostsResponeseDto::new).collect(Collectors.toList());
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
 
--- src/main/java/com/june/springboot/web/PostsApiController.java
+++ src/main/java/com/june/springboot/web/PostsApiController.java
@@ -29,6 +29,4 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
-
-
 }
